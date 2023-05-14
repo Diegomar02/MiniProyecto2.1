52a02f8b77924eedb404494b3f7fdcee7d211a40
Diegomar02 <118679780+Diegomar02@users.noreply.github.com>
2023-05-14 09:42:56 -0600

    boton sesion

--- MiniProyecto6semestre/src/app/app.module.ts
+++ MiniProyecto6semestre/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -38,7 +39,8 @@ import { AyudaComponent } from './ayuda/ayuda.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- MiniProyecto6semestre/src/app/boton-sesion/boton-sesion.component.ts
+++ MiniProyecto6semestre/src/app/boton-sesion/boton-sesion.component.ts
@@ -34,7 +34,6 @@ export class BotonSesionComponent {
     }
     this.verificarSesion();
   }
-  
 
   verificarSesion(): void {
     if (localStorage.getItem("usuarioActual") != null) {
